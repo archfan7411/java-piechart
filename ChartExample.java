d58fb8810309ae379093c59cf144213c431f1400
archfan <33993466+archfan7411@users.noreply.github.com>
2018-12-19 13:54:41 -0500

    Add example and image

---
+++ ChartExample.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class ChartExample extends JFrame
+{
+    public ChartExample()
+    {
+        Container c = getContentPane();
+        c.setBackground(Color.WHITE);
+        PieChart chart = new PieChart(30, 50, 100);
+        chart.AddElement("Amount of red in this chart", 134, Color.RED);
+        chart.AddElement("Amount of green in this chart", 483, Color.GREEN);
+        chart.AddElement("Amount of yellow in this chart", 47, Color.YELLOW);
+        c.add(chart);
+    }
+
+    public static void main()
+    {
+        ChartExample w = new ChartExample();
+        w.setBounds(300, 300, 500, 330);
+        w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        w.setVisible(true);
+    }
+}
